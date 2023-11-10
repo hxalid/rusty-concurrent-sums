76ffb5aa664b4477d86e6ace82527112561e4b92
Khalid Hasanov <4573868+hxalid@users.noreply.github.com>
2023-11-10 23:05:51 +0000

    feat: add concurrent sum in crossbeam and reformat
    Signed-off-by: Khalid Hasanov <4573868+hxalid@users.noreply.github.com>

--- src/lib.rs
+++ src/lib.rs
@@ -1,75 +1,10 @@
-use std::cmp::min;
-use std::ops::AddAssign;
-use std::sync::Arc;
-use std::thread::spawn;
+#![allow(dead_code)]
+#![deny(missing_docs)]
+#![forbid(unsafe_code)]
+#![warn(missing_debug_implementations)]
 
-fn concurrent_sum<T>(numbers: Vec<T>, num_threads: usize) -> T
-where
-    T: AddAssign + Copy + Default + Send + Sync + 'static,
-{
-    if num_threads == 0 || num_threads > numbers.len() {
-        panic!(
-            "The number of threads should be between [1, {}], given: {}",
-            numbers.len(),
-            num_threads
-        );
-    }
+//! Concurrent Vector sum implementation in Rust
+//! using different approaches and crates.
 
-    if numbers.is_empty() {
-        return T::default();
-    }
-
-    let size_per_thread = (numbers.len() - 1) / num_threads + 1;
-    let arc_nums = Arc::new(numbers);
-    let mut threads = Vec::with_capacity(num_threads);
-
-    for i in 0..num_threads {
-        let nums = arc_nums.clone();
-
-        let thread = spawn(move || {
-            let from = i * size_per_thread;
-            let to = min(from + size_per_thread, nums.len());
-
-            let mut sum = T::default();
-            for num in &nums[from..to] {
-                sum += *num;
-            }
-
-            sum
-        });
-
-        threads.push(thread);
-    }
-
-    let mut sum = T::default();
-    for t in threads {
-        sum += t.join().expect("panic in thread");
-    }
-    sum
-}
-
-#[cfg(test)]
-mod tests {
-    use super::*;
-
-    #[test]
-    fn sum_valid() {
-        let vec = vec![1, 2, 3, 4, 5, 6, 7, 8, 9];
-        let sequential: u64 = vec.iter().cloned().sum();
-        assert_eq!(sequential, concurrent_sum(vec, 2));
-    }
-
-    #[test]
-    #[should_panic]
-    fn sum_panic_more_threads() {
-        let vec = vec![1];
-        _ = concurrent_sum(vec, 2);
-    }
-
-    #[test]
-    #[should_panic]
-    fn sum_panic_zero_threads() {
-        let vec = vec![1];
-        _ = concurrent_sum(vec, 0);
-    }
-}
+pub mod concurrent_sum_crossbeam;
+pub mod concurrent_sum_std;
