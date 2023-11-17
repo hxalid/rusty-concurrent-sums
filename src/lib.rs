3cb443ac64f004bb115c2c89835b7c06418b3838
Khalid Hasanov <4573868+hxalid@users.noreply.github.com>
2023-11-17 21:39:39 +0000

    feat: add feature builds (#3)
    * feat: add feature builds
    * feat: add cargo clippy into ci
    
    Signed-off-by: Khalid Hasanov <4573868+hxalid@users.noreply.github.com>
    
    ---------
    
    Signed-off-by: Khalid Hasanov <4573868+hxalid@users.noreply.github.com>

--- src/lib.rs
+++ src/lib.rs
@@ -6,6 +6,11 @@
 //! Concurrent Vector sum implementation in Rust
 //! using different approaches and crates.
 
+#[cfg(feature = "crossbeam")]
 pub mod concurrent_sum_crossbeam;
+
+#[cfg(feature = "rayon")]
 pub mod concurrent_sum_rayon;
+
+#[cfg(feature = "std")]
 pub mod concurrent_sum_std;
